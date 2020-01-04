9523c7a312a8ae23ec97d28e305be2d65c8a44d7
Jennifer Winer <jeniwiner@gmail.com>
2020-01-04 11:04:56 -0700

    updates

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 
 const List: React.FC = () => {
+  let [ fruit, setFruit ] = useState<string[]>([]);
+  let [ inputFruit, setInputFruit ] = useState('');
 
-    let fruit : string[] = [];
-    return (
-    <ul>
-        {
-            fruit.length != 0 && fruit.map((item: string) => <li>{item}</li>)
-        }
-    </ul>
+  useEffect(() => {
+    console.log(inputFruit)
+  }, [inputFruit]);
+  
+  const handleSmoothie = () => {
+    //let updatedFruits = [ ...fruit, newFruit];
+    //setFruit(updatedFruits);
+  }
+  const addInputFruit = (e:Event) => {
+      console.log(e)
+  }
+
+  return (
+    <div>
+      <input placeholder="Enter a fruit" onChange={addInputFruit(e)}/>
+      <button onClick={handleSmoothie}>Smoothie!</button>
+      <ul>{ fruit.length != 0 && fruit.map((item: string) => <li>{item}</li>) }</ul>
+    </div>
   );
 }
 
